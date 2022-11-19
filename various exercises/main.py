78963b9b75e9178837d08623b7c384c76e7b3e09
vedad92 <96484685+vedad92@users.noreply.github.com>
2022-11-19 15:13:37 +0100

    Update main.py

--- various exercises/main.py
+++ various exercises/main.py
@@ -1,14 +1,9 @@
-# Do not modify these lines
+
 from ast import Try
 from inspect import getclosurevars
 import os
 from socket import if_nametoindex
 
-__winc_id__ = "534d85ea1ab14924a91f9eccf6f3f30d"
-__human_name__ = "errors"
-
-
-# Test your functions here to make sure the functionality remains the same.
 def main():
     foo = list(range(10))
     print(
@@ -19,12 +14,6 @@ def main():
     ...
 
 
-
-"""Change the three functions below from Look Before You Leap (LBYL) to Easier
-to Ask for Forgiveness than Permission (EAFP)"""
-
-
-# Returns the addition of x and y if it's defined, otherwise returns 0
 def add(x, y):
     try:
         return x + y
@@ -32,16 +21,12 @@ def add(x, y):
         return 0
                     
 
-# Returns the contents of the file at 'filename', or an empty string if the
-# file does not exist
 def read_file(filename):
     try:
         return open(filename, "r").read()
     except FileNotFoundError:
         return ""
 
-
-# Returns item at `index` from list `l` if possible, otherwise returns None
 def get_item_from_list(l, index):
     max_index = len(l) - 1
     min_index = -1 - max_index
@@ -52,17 +37,12 @@ def get_item_from_list(l, index):
     except TypeError:
         return None
 
-print(get_item_from_list("456",'55'))
-
-
-
+print(get_item_from_list("456",'55'))    
     
-    
-    
-''' if index <= max_index and index >= min_index:
+if index <= max_index and index >= min_index:
         return l[index]
     else:
-        return None'''
+        return None
 
 
 if __name__ == "__main__":
